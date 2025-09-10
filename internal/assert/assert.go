9af548db991c84872d4ef6095951d8eb347acce9
based <thebasedcoder@proton.me>
2025-09-10 10:04:22 +0330

    finish

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -1,11 +1,21 @@
 package assert
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func Equal[T comparable](t *testing.T, expected, actual T) {
 	t.Helper()
 
 	if expected != actual {
 		t.Errorf("expected: %v, actual: %v", expected, actual)
+	}
+}
+
+func StringContains(t *testing.T, actual, expectedSubstring string) {
+	t.Helper()
+	if !strings.Contains(actual, expectedSubstring) {
+		t.Errorf("got: %q; expected to contain: %q", actual, expectedSubstring)
 	}
 }
